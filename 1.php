dc005c350db21999afdd0882cbedd732315aafa2
avraham david <84441010+avraham-david@users.noreply.github.com>
2025-04-24 20:22:02 +0300

    Update 1.php

--- 1.php
+++ 1.php
@@ -5,7 +5,6 @@ $resend = Resend::client('re_iC81sQvL_2bmsWYoPWWtL7Rs9M2NhgGrs');
 $resend->emails->send([
   'from' => 'onboarding@resend.dev',
   'to' => 'tcrvo1708@gmail.com',
-  'subject' => 'משתמש נכנס',
-  'html' => 'משתמש נכנס'
+  'subject' => 'Hello World',
+  'html' => '<p>Congrats on sending your <strong>first email</strong>!</p>'
 ]);
-?>
