c3cb886bd6fd1955476683586d5664bf656dfd1a
avraham david <84441010+avraham-david@users.noreply.github.com>
2025-04-24 22:30:20 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,6 @@
 <?php
+// קוד זה יקרא את המפתח מתוך משתני הסביבה
 return [
-    "OPENAI_API_KEY" => "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" // כאן תכניס את המפתח שלך
+    'OPENAI_API_KEY' => getenv('OPENAI_API_KEY') // משתנה סביבה של המפתח
 ];
+?>
