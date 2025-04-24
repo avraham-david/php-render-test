25574593375fd801e3f9a535962e26e266fef0dc
avraham david <84441010+avraham-david@users.noreply.github.com>
2025-04-24 22:11:04 +0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+return [
+    "OPENAI_API_KEY" => "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" // כאן תכניס את המפתח שלך
+];
