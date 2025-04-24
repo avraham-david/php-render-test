2e3609d3820a41b7588197cfcfd0c5fb3677ce17
avraham david <84441010+avraham-david@users.noreply.github.com>
2025-04-24 20:14:49 +0300

    Create 1.php

---
+++ 1.php
@@ -0,0 +1,10 @@
+<?php
+
+$resend = Resend::client('re_iC81sQvL_2bmsWYoPWWtL7Rs9M2NhgGrs');
+
+$resend->emails->send([
+  'from' => 'onboarding@resend.dev',
+  'to' => 'tcrvo1708@gmail.com',
+  'subject' => 'משתמש נכנס',
+  'html' => 'משתמש נכנס'
+]);
