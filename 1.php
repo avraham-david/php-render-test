a8dfae456a5529ef27791b61ae052ce8e28c7131
avraham david <84441010+avraham-david@users.noreply.github.com>
2025-04-24 21:51:00 +0300

    Update 1.php

--- 1.php
+++ 1.php
@@ -5,7 +5,7 @@ $url = 'https://api.resend.com/emails';
 
 // נתוני המייל
 $data = [
-    'from' => 'av@av.av',
+    'from' => 'ad@resend.dev',
     'to' => ['tcrvo1708@gmail.com'],
     'subject' => 'hello world',
     'html' => '<p>it works!</p>',
